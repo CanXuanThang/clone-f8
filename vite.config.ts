ce17cb88b518870f40c34dad73d82406d8cd9082
canxuanthang2001tt@gmail.com <canxuanthang2001tt@gmail.com>
2023-11-02 15:18:14 +0700

    confix base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
+
+/** module path */
+import tsPaths from './tsconfig.path.json';
+
+/** Resolve tsconfig.json paths to aliases */
+function resolveTsconfigPathsToAlias() {
+    const paths = tsPaths.compilerOptions.paths;
+    return Object.keys(paths).reduce((alias, item) => {
+        const key = item.replace('/*', '');
+        alias[key] = path.resolve(__dirname, paths[item][0].replace('/*', ''));
+        return alias;
+    }, {});
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return defineConfig({
+        plugins: [react()],
+        server: {
+            host: process.env.VITE_APP_HOST,
+        },
+        resolve: {
+            alias: resolveTsconfigPathsToAlias(),
+        },
+    });
+};
